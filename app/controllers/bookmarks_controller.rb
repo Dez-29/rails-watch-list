15405de5d9ad33aa3a8e7f8f10605f4b0c2fca66
Paul Deze <paulo.dze1@gmail.com>
2021-06-10 18:09:26 +0200

    Watch List

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @movie = Movie.new
+    @bookmark = Bookmark.new
+  end
+  
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.movie = @movie
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie, :list)
+  end
 end
